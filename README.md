f97cd507566e55b44890415b477f8f2f6c7d5c2a
javier-vaquero <javi_salamanca@hotmail.com>
2015-06-08 16:56:07 +0200

    Tercer "commit": licencia y leeme

---
+++ README.md
@@ -0,0 +1,7 @@
+# random
+**Superaplicación** que calcula:
+
+* Un número aleatorio.
+* Si el numero es mayor o menor de 0.5.
+
+El codigo fuente esta [aqui](https://github.com/javier-vaquero/random).
--- random.js
+++ random.js
@@ -1,3 +1,19 @@
+/**
+ * Copyright (c) 2014-2015, Javier Vaquero
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * required by applicable law or agreed to in writing, software
+ * under the License is distributed on an "AS IS" BASIS,
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * the License for the specific language governing permissions and
+ * under the License.
+ */
+
 // Math.random() devuelve número aleatorio entre 0 y 1.
 var numero = Math.random();
 
